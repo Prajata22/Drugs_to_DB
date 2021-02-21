9823d951903ef84b4f581c1d02b62e1dc255a752
Prajata Samanta <prajatasamanta2204@gmail.com>
2021-02-21 16:33:16 +0530

    Initial commit

---
+++ app/src/main/java/com/applex/drugs_to_db/DatabaseHelper.java
@@ -0,0 +1,91 @@
+package com.applex.drugs_to_db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME ="Drugs_A.db";
+    private static final String TABLE_NAME = "Drugs_A_Table";
+    private static final String col2 = "NAME";
+    private static final String col3 = "MANUFACTURER";
+    private static final String col4 = "CONTENTS";
+    private static final String col5 = "CIMS_CLASS";
+    private static final String col6 = "ATC_CLASSIFICATION";
+    private static final String col7 = "FORM_1";
+    private static final String col8 = "PACKING_PRICE_1";
+    private static final String col9 = "FORM_2";
+    private static final String col10 = "PACKING_PRICE_2";
+    private static final String col11 = "FORM_3";
+    private static final String col12 = "PACKING_PRICE_3";
+    private static final String col13 = "FORM_4";
+    private static final String col14 = "PACKING_PRICE_4";
+    private static final String col15 = "FORM_5";
+    private static final String col16 = "PACKING_PRICE_5";
+    private static final String col17 = "FORM_6";
+    private static final String col18 = "PACKING_PRICE_6";
+    private static final String col19 = "FORM_7";
+    private static final String col20 = "PACKING_PRICE_7";
+    private static final String col21 = "FORM_8";
+    private static final String col22 = "PACKING_PRICE_8";
+    private static final String col23 = "FORM_9";
+    private static final String col24 = "PACKING_PRICE_9";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + col2 + " TEXT, " + col3 + " TEXT, " + col4 + " TEXT, " + col5 + " TEXT, " + col6 + " TEXT, " + col7 + " TEXT, " + col8 +
+                " TEXT, " + col9 + " TEXT, " + col10 + " TEXT, " + col11 + " TEXT, " + col12 + " TEXT, " + col13 + " TEXT, " + col14 +
+                " TEXT, " + col15 + " TEXT, " + col16 + " TEXT, " + col17 + " TEXT, " + col18 + " TEXT, " + col19 + " TEXT, " + col20 +
+                " TEXT, " + col21 + " TEXT, " + col22 + " TEXT, " + col23 + " TEXT, " + col24 + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+    }
+
+    public boolean addData(String name, String manufacturer, String contents, String cims_class, String atc_classification,
+                           String form_1, String packing_price_1, String form_2, String packing_price_2,
+                           String form_3, String packing_price_3, String form_4, String packing_price_4,
+                           String form_5, String packing_price_5, String form_6, String packing_price_6,
+                           String form_7, String packing_price_7, String form_8, String packing_price_8,
+                           String form_9, String packing_price_9) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col2, name);
+        contentValues.put(col3, manufacturer);
+        contentValues.put(col4, contents);
+        contentValues.put(col5, cims_class);
+        contentValues.put(col6, atc_classification);
+        contentValues.put(col7, form_1);
+        contentValues.put(col8, packing_price_1);
+        contentValues.put(col9, form_2);
+        contentValues.put(col10, packing_price_2);
+        contentValues.put(col11, form_3);
+        contentValues.put(col12, packing_price_3);
+        contentValues.put(col13, form_4);
+        contentValues.put(col14, packing_price_4);
+        contentValues.put(col15, form_5);
+        contentValues.put(col16, packing_price_5);
+        contentValues.put(col17, form_6);
+        contentValues.put(col18, packing_price_6);
+        contentValues.put(col19, form_7);
+        contentValues.put(col20, packing_price_7);
+        contentValues.put(col21, form_8);
+        contentValues.put(col22, packing_price_8);
+        contentValues.put(col23, form_9);
+        contentValues.put(col24, packing_price_9);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+}
---
+++ app/src/main/java/com/applex/drugs_to_db/MainActivity.java
@@ -0,0 +1,103 @@
+package com.applex.drugs_to_db;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> new PutDataIntoDB().execute());
+    }
+
+    public String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("Drugs_A.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            Log.e("BAMCHIKI", e.getMessage());
+            return null;
+        }
+        return json;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class PutDataIntoDB extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Copying you data");
+            progressDialog.setMessage("Please wait...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                JSONArray jsonArray = new JSONArray(loadJSONFromAsset());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+
+//                    Log.i("BAMCHIKI", obj.getString("NAME") + obj.getString("MANUFACTURER") + obj.getString("CONTENTS") +
+//                            obj.getString("CIMS CLASS") + obj.getString("ATC CLASSIFICATION") + obj.getString("FORM(1)") +
+//                            obj.getString("PACKING/PRICE(1)") + obj.getString("FORM(2)") + obj.getString("PACKING/PRICE(2)") +
+//                            obj.getString("FORM(3)") + obj.getString("PACKING/PRICE(3)") + obj.getString("FORM(4)") +
+//                            obj.getString("PACKING/PRICE(4)") + obj.getString("FORM(5)") + obj.getString("PACKING/PRICE(5)") +
+//                            obj.getString("FORM(6)") + obj.getString("PACKING/PRICE(6)") + obj.getString("FORM(7)") +
+//                            obj.getString("PACKING/PRICE(7)") + obj.getString("FORM(8)") + obj.getString("PACKING/PRICE(8)") +
+//                            obj.getString("FORM(9)") + obj.getString("PACKING/PRICE(9)"));
+
+                    databaseHelper.addData(obj.getString("NAME"), obj.getString("MANUFACTURER"), obj.getString("CONTENTS"),
+                            obj.getString("CIMS CLASS"), obj.getString("ATC CLASSIFICATION"), obj.getString("FORM(1)"),
+                            obj.getString("PACKING/PRICE(1)"), obj.getString("FORM(2)"), obj.getString("PACKING/PRICE(2)"),
+                            obj.getString("FORM(3)"), obj.getString("PACKING/PRICE(3)"), obj.getString("FORM(4)"),
+                            obj.getString("PACKING/PRICE(4)"), obj.getString("FORM(5)"), obj.getString("PACKING/PRICE(5)"),
+                            obj.getString("FORM(6)"), obj.getString("PACKING/PRICE(6)"), obj.getString("FORM(7)"),
+                            obj.getString("PACKING/PRICE(7)"), obj.getString("FORM(8)"), obj.getString("PACKING/PRICE(8)"),
+                            obj.getString("FORM(9)"), obj.getString("PACKING/PRICE(9)"));
+                }
+            } catch (JSONException e) {
+                Log.e("BAMCHIKI", e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(progressDialog != null && progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+            Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
